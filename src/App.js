d766c9a5a38a2f125d70226c8fd96569429650ef
Varun Bhatt <varun.bhatt@teachforindia.org>
2019-09-06 00:55:09 +0530

    Add firebase, styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+import TeamManager from './TeamManager'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="h6">MetaFoot</Typography>
+				</Toolbar>
+			</AppBar>
+			<main>
+				<TeamManager></TeamManager>
+			</main>
+		</div>
+	)
 }
 
-export default App;
+export default App
