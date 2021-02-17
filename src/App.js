87b65850f0eecbb86cac56663b90414177b0e1de
Varun Bhatt <varun.bhatt@teachforindia.org>
2021-02-17 18:52:45 +0530

    Fix #6 show update when new version of app is available

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 
 import {
@@ -9,8 +9,12 @@ import {
 	secondaryDark,
 	secondaryLight,
 } from './common/colours'
+import Snackbar from '@material-ui/core/Snackbar'
+import Button from '@material-ui/core/Button'
 
 import TeamManager from './TeamManager'
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const theme = createMuiTheme({
 	palette: {
@@ -28,6 +32,31 @@ const theme = createMuiTheme({
 })
 
 function App() {
+	const [isSnackbarOpen, setSnackbarOpen] = useState(false)
+	const [waitingWorker, setWaitingWorker] = useState(null)
+	useEffect(() => {
+		serviceWorkerRegistration.register({
+			onUpdate: registration => {
+				setWaitingWorker(registration?.waiting)
+			},
+		})
+	}, [])
+
+	useEffect(() => {
+		if (waitingWorker) {
+			setSnackbarOpen(true)
+		}
+	}, [waitingWorker])
+
+	function handleClose() {
+		setSnackbarOpen(false)
+	}
+	function updateServiceWorker() {
+		if (waitingWorker) {
+			waitingWorker.postMessage({ type: 'SKIP_WAITING' })
+			window.location.reload()
+		}
+	}
 	return (
 		<MuiThemeProvider theme={theme}>
 			<div className="App">
@@ -35,6 +64,25 @@ function App() {
 					<TeamManager />
 				</main>
 			</div>
+			<Snackbar
+				anchorOrigin={{
+					vertical: 'bottom',
+					horizontal: 'left',
+				}}
+				open={isSnackbarOpen}
+				autoHideDuration={null}
+				onClose={handleClose}
+				message="A new version of this app is availables"
+				action={
+					<Button
+						color="secondary"
+						size="small"
+						onClick={updateServiceWorker}
+					>
+						Update
+					</Button>
+				}
+			/>
 		</MuiThemeProvider>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-serviceWorkerRegistration.register()
 //reportWebVitals()
